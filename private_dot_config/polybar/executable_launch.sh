cd04a6872fbdd46dcc0ebdebe464d2de0e2f3836
purplehaze <yosuke.otosu@gmail.com>
2024-02-25 05:02:35 +0900

    Add i3 window manager

---
+++ private_dot_config/polybar/executable_launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 0.1; done
+
+# Launch bar
+polybar main &
+# polybar HDMI-0 & polybar DP-1 &
+
+echo "Bars launched..."
